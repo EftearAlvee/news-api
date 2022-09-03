a32fa08cd4134fa2924e4effa4e7f72dda4213f3
Eftear Alvee <EftearAlvee>
2022-09-03 09:35:30 +0600

    adding

---
+++ js/news.js
@@ -0,0 +1,52 @@
+//Vaeriables
+const homeBtn = document.getElementById("home");
+const breakingNewsBtn = document.getElementById("breaking");
+const regularNewsBtn = document.getElementById("regular");
+const internationalnewsBtn = document.getElementById("international");
+const sportsNewsBtn = document.getElementById("sports");
+const entertainmentNewsBtn = document.getElementById("entertainment");
+const cultureNewsBtn = document.getElementById("culture");
+const artsNewsBtn = document.getElementById("arts");
+const allNewsBtn = document.getElementById("all");
+
+//api's
+const HOME_NEWS = "https://openapi.programming-hero.com/api/news/categories";
+const BRERAKING_NEWS = "https://openapi.programming-hero.com/api/news/category/01";
+const REGULAR_NEWS = "https://openapi.programming-hero.com/api/news/category/02";
+const INTERNATIONAL_NEWS = "https://openapi.programming-hero.com/api/news/category/03";
+const SPORTS_NEWS = "https://openapi.programming-hero.com/api/news/category/04"
+const ENTERTAINMENT_NEWS = "https://openapi.programming-hero.com/api/news/category/05";
+const CULTURE_NEWS = "https://openapi.programming-hero.com/api/news/category/06";
+const ARTS_NEWS = "https://openapi.programming-hero.com/api/news/category/07";
+const ALL_NEWS = "https://openapi.programming-hero.com/api/news/category/08";
+
+homeBtn.addEventListener("click", function () {
+
+})
+breakingNewsBtn.addEventListener("click", function () {
+
+})
+regularNewsBtn.addEventListener("click", function () {
+
+})
+internationalnewsBtn.addEventListener("click", function () {
+
+})
+sportsNewsBtn.addEventListener("click", function () {
+
+})
+entertainmentNewsBtn.addEventListener("click", function () {
+
+})
+homeBtn.addEventListener("click", function () {
+
+})
+cultureNewsBtn.addEventListener("click", function () {
+
+})
+artsNewsBtn.addEventListener("click", function () {
+
+})
+allNewsBtn.addEventListener("click", function () {
+
+})
